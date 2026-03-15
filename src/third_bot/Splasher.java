ab798059101adc46ff2f0177cdfef671bac522b5
MichaelJamesL <jamesliman7@gmail.com>
2026-03-15 19:14:25 +0700

    chore: align bot name to convention

---
+++ src/third_bot/Splasher.java
@@ -0,0 +1,16 @@
+package third_bot;
+
+import battlecode.common.*;
+
+public final class Splasher extends Robot {
+
+  public Splasher(RobotController rc_) throws GameActionException {
+    super(rc_);
+  }
+
+  // No Splasher
+
+  public void doMicro() {}
+  
+  public void doMacro() {}
+}
---
+++ src/third_bot/util/Stack.java
@@ -0,0 +1,40 @@
+package third_bot.util;
+
+/**
+ * Simple fixed size stack helper class
+ */
+public class Stack<T> {
+  
+  private T[] data;
+  private int index = 0;
+  
+  public Stack(int max_size) {
+    data = (T[]) new Object[max_size];
+  }
+
+  public boolean push(T item) {
+    if (index == data.length) { return false; }
+    data[index++] = item;
+    return true;
+  }
+
+  public T pop() {
+    return (index == 0 ? null : data[--index]);
+  }
+
+  public T top() {
+    return (index == 0 ? null : data[index - 1]);
+  }
+
+  public T bottom() {
+    return (index == 0 ? null : data[0]);
+  }
+
+  public int capacity() { return data.length; }
+  public int used() { return index; }
+  public int available() { return data.length - index; }
+  public boolean empty() { return index == 0; }
+  public void clear() { index = 0; }
+}
+
+// Credits: justinottesen/battlecode25
